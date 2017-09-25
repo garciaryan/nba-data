08b5456fcd472ebbe1dc2f3954b28282309a8784
Ryan Garcia <garciaryan007@yahoo.com>
2017-09-25 16:06:16 -0700

    restarted project with postgresql

--- db.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://garciaryan:4cC6^UdYKt@ds149724.mlab.com:49724/nba-stats')
-
-db = client['nba-data']
-
-print('it ran!')
