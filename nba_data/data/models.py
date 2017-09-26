b79432306f1a92e7b3a6a5c9f0bd1beaa5b0bdcc
Ryan Garcia <garciaryan007@yahoo.com>
2017-09-26 14:27:52 -0700

    server works

--- nba_data/data/models.py
+++ nba_data/data/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    minutes = models.DecimalField(max_digits=3, decimal_places=1)
+    pts = models.DecimalField(max_digits=3, decimal_places=1)
+    fgm = models.DecimalField(max_digits=3, decimal_places=1)
+    fga = models.DecimalField(max_digits=3, decimal_places=1)
+    threes_m = models.DecimalField(max_digits=3, decimal_places=1)
+    threes_a = models.DecimalField(max_digits=3, decimal_places=1)
+    reb = models.DecimalField(max_digits=3, decimal_places=1)
+    ast = models.DecimalField(max_digits=3, decimal_places=1)
+    stl = models.DecimalField(max_digits=3, decimal_places=1)
+    blk = models.DecimalField(max_digits=3, decimal_places=1)
+    to = models.DecimalField(max_digits=3, decimal_places=1)
