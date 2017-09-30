2da96a2d7cfc90ef49c27c79cd37da54f33ed644
Ryan Garcia <garciaryan007@yahoo.com>
2017-09-30 12:27:54 -0700

    adds scrape functionality

--- nba_data/nba_data/nba_data/spiders/stats_spider.py
+++ nba_data/nba_data/nba_data/spiders/stats_spider.py
@@ -6,5 +6,14 @@ class StatsSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://stats.nba.com/players'
+            'https://stats.nba.com/leaders'
         ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'stats-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
