5906a6d14a79d8be711d6b1d3c02a369eac53629
Ryan Garcia <garciaryan007@yahoo.com>
2017-10-11 13:17:50 -0700

    selenium and scrapy working together

--- nba_data/nba_data/nba_data/spiders/stats_spider.py
+++ nba_data/nba_data/nba_data/spiders/stats_spider.py
@@ -1,19 +1,35 @@
 import scrapy
+import time
+from selenium import webdriver
 
 
 class StatsSpider(scrapy.Spider):
     name = "stats"
+    allowed_domains = ["stats.nba.com"]
+    start_urls = ('https://stats.nba.com/leaders',)
 
-    def start_requests(self):
-        urls = [
-            'https://stats.nba.com/leaders'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(7)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'stats-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.driver.get(response.url)
+
+        while True:
+            next = self.driver.find_element_by_tag_name('tr')
+
+            try:
+                page = response.url.split("/")[-2]
+                filename = 'stats-%s.html' % page
+                with open(filename, 'wb') as f:
+                    f.write(response.body)
+                time.sleep(7)
+                next.click()
+            except:
+                break
+
+    #def parse_stats(self, response):
+
+
+
+
